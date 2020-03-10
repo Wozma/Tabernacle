a9d01d3078e7ce32aefb3540652b4ba6197e985f
mawoz <martin.gauchet@hotmail.fr>
2020-03-10 16:00:17 +0100

    le seveur semble marcher

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-const express = require('express');
+/* const express = require('express');
 const app = express();
-const http = require('http').Server(app);
-const port = process.env.PORT || 3000;
 const internalIp = require('internal-ip');
 const figlet = require('figlet');
 const axios = require('axios');
@@ -10,16 +8,44 @@ const URL_API = "https://api.themoviedb.org/3/movie/76341?api_key=d81f509ee99573
 
 app.set('view engine', 'ejs');
 
+app.get('/', function (req, res) {
+    axios.get(URL_API)
+        .then(response => {
+            let tabernacle = response.data
+            res.render('index', { tabernacle: tabernacle });
+        })
+        .catch(error => {
+            console.log(error);
+        });
+})
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+http.listen(port, function () {
+    figlet('Tabernacle', function (err, data) {
+        if (err) {
+            console.log('Something went wrong...');
+            console.dir(err);
+            return;
+        }
+        console.log("\x1b[36m", data)
+        console.log("\x1b[36m", `> http://localhost:${port}`)
+        console.log("\x1b[32m", `> http://${internalIp.v4.sync()}:${port}`)
+    });
 });
+  */
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+var http = require('http');
+const express = require('express');
+const app = express();
+const internalIp = require('internal-ip');
+const figlet = require('figlet');
+const axios = require('axios');
+
+const URL_API = "https://api.themoviedb.org/3/movie/76341?api_key=d81f509ee99573f10007ecc04<7db542c";
+
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+    res.render("index" , {prenom: "thomas"})
+})
+
+app.listen(8080);
