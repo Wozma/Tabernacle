a3721b9ef18385c37398f91c5721c2cb0348158b
mawoz <martin.gauchet@hotmail.fr>
2020-03-12 11:00:21 +0100

    Thomas a réparé

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const internalIp = require('internal-ip');
 const figlet = require('figlet');
 const axios = require('axios');
 // must be listed before other Firebase SDKs
-const firebase = require("firebase/app");
-const MovieDB = require('moviedb')('d81f509ee99573f10007ecc04<7db542c');
 
 // Add the Firebase products that you want to use
 require("firebase/auth");
@@ -14,28 +12,37 @@ require("firebase/firestore");
 
 
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
-MovieDB.searchMovie({ query: 'Alien' }, (err, res) => {
-    console.log(res);
-  });
+/*
+get fim details
+https://api.themoviedb.org/3/movie/{id_film}?api_key=b776dda5347f7b2bdc52252573f4e78c&language=en-US
+*/
+
+var selectFilmsID = [
+    "90",
+    "12",
+    "91",
+    "80"
+]
+
+const API_KEY_TMDB = "b776dda5347f7b2bdc52252573f4e78c"
 
 
 app.get('/', (req, res) => {
-    res.render("accueil");
+    let randomNumber = Math.floor(Math.floor(Math.random() * selectFilmsID.length))
+    let selectRandomFilm = selectFilmsID[randomNumber]
+
+    axios.get(`https://api.themoviedb.org/3/movie/${selectRandomFilm}?api_key=${API_KEY_TMDB}&language=en - US`)
+        .then(response => {
+            let film = response.data
+            res.render("accueil", { film: film });
+        })
 });
 
 app.get('/index', (req, res, next) => {
     res.render("index");
 });
 
-app.get('/', function (req, res) {
-    axios.get(URL_API)
-        .then(response => {
-            res.render('index', { movie : movie });
-        })
-        .catch(error => {
-            console.log(error);
-        });
-})
 
 app.listen(8080);   
