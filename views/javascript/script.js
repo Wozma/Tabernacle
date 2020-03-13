c1acc70c651d7dd011c5363a5a8481de862a5599
mawoz <martin.gauchet@hotmail.fr>
2020-03-13 15:43:26 +0100

    un seul fichier js

---
+++ views/javascript/script.js
@@ -0,0 +1,13 @@
+import { app } from "firebase";
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyAUUIQDPQFnNLFY1Mmp-7x4yZLwD7vE5ko",
+    authDomain: "tabernacledb.firebaseapp.com",
+    databaseURL: "https://tabernacledb.firebaseio.com",
+    projectId: "tabernacledb",
+    storageBucket: "tabernacledb.appspot.com",
+    messagingSenderId: "385139316104",
+    appId: "1:38513931610 4:web:d8b28eee314219fe8bd63d"
+  };
+  firebase.initializeApp(firebaseConfig);
+
